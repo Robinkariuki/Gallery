a7f13c1a828a641bbfa156d452da9e48e6898959
Robin <robinkaris7@gmail.com>
2019-10-05 19:58:59 +0300

    this commit adds bootsrap

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- images/views.py
+++ images/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def images(request):
+
+    
+
+    return render(request,'images.html')
