58fc57f2c7f019d1c05f20c6325940c41c9689d6
Robin <robinkaris7@gmail.com>
2019-10-06 08:20:46 +0300

    this commit modifies images.html

--- images/urls.py
+++ images/urls.py
@@ -4,3 +4,5 @@ from . import views
 urlpatterns=[
      url(r'^$',views.images,name='Images'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
